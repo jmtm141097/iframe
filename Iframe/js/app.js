dec3c9f0fc6daa6840302d22239f83dcc555ef09
Juan Tamayo <jmtm141097@gmail.com>
2021-06-24 08:42:25 -0500

    feat: capturar tk y optimizacion

--- Iframe/js/app.js
+++ Iframe/js/app.js
@@ -1,17 +1,16 @@
-// let token = getParameterByName('tk');
-// console.log(token);
+let detector = new MobileDetect(window.navigator.userAgent);
+const startTime = new Date();
 const API = axios.create({
     baseURL: "http://127.0.0.1:4000/v1"
 });
 
-let detector = new MobileDetect(window.navigator.userAgent)
+const tk = window.location.search;
+const urlParams = new URLSearchParams(tk);
 
 let ip;
 let city;
-
 async function getCityAndIP() {
     await geoip2.city((data) => {
-        console.log(data);
         ip = data.traits.ip_address;
         city = data.city.names.es + " - " + data.subdivisions[0].names.en + " - " + data.country.names.es;
     }, (error) => {
@@ -34,7 +33,6 @@ function device() {
     else return "Ordenador";
 }
 
-const startTime = new Date();
 async function sendData() {
     const URL = "/activity/toRegister";
     const endTime = new Date();
@@ -48,7 +46,7 @@ async function sendData() {
         browser: (detector.userAgent()) ? detector.userAgent() : sBrowser,
         platform: (detector.os()) ? detector.os() : window.navigator.platform,
         lenguage: window.navigator.language,
-        token: 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MjI3NTA1NTAsImV4cCI6MTYyNTM0MjU1MH0.LwQuJ9digxk_XLAmstOhIYUOBgXX6_XS0kbFZGIYh9-TtWB8S-Gt5aMSI_tDAXuEoq16MjcPoleEwpkhDjIPSA'
+        token: urlParams.get('tk')
     };
     await API.post(URL, activity, {
             headers: {
@@ -66,29 +64,3 @@ async function sendData() {
 }
 
 window.addEventListener("beforeunload", sendData, false);
-
-// Localizacion.
-// const funcionInit = () => {
-//     if (!"geolocation" in navigator) {
-//         return alert("Tu navegador no soporta el acceso a la ubicación. Intenta con otro");
-//     }
-
-//     const onUbicacionConcedida = ubicacion => {
-//         console.log("Tengo la ubicación: ", ubicacion);
-//     }
-
-//     const onErrorDeUbicacion = err => {
-//         console.log("Error obteniendo ubicación: ", err);
-//     }
-
-//     const opcionesDeSolicitud = {
-//         enableHighAccuracy: false, // Alta precisión
-//         maximumAge: 0, // No queremos caché
-//         timeout: 5000 // Esperar solo 5 segundos
-//     };
-//     // Solicitar
-//     navigator.geolocation.getCurrentPosition(onUbicacionConcedida, onErrorDeUbicacion, opcionesDeSolicitud);
-
-// };
-
-// funcionInit();
